ae723affdb4682c7a1324d6131d1b2da06f592be
GaryOcean428 <braden.lang77@gmail.com>
2024-12-24 09:35:34 +0800

    d

--- auth0/01-login/auth0-config.ts
+++
@@ -1,9 +0,0 @@
-export const auth0Config = {
-  domain: 'dev-5w0kgzrm.us.auth0.com',
-  clientId: 'yklLf2mJYPqMw8uernqwkHEyfMGpxVia',
-  authorizationParams: {
-    redirect_uri: 'http://localhost:3000',
-    audience: 'https://dev-5w0kgzrm.us.auth0.com/api/v2/',
-    scope: 'openid profile email'
-  }
-};
