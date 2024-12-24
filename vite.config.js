98fe11ef1893e62eb09891f3b21cf12b8e86217f
GaryOcean428 <braden.lang77@gmail.com>
2024-12-24 09:41:44 +0800

    d

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,23 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  return {
-    plugins: [
-      react(),
-      visualizer({
-        filename: 'stats.html',
-        open: true,
-        gzipSize: true,
-        brotliSize: true,
-      })
-    ],
-    define: {
-      // Expose env variables to your client-side code
-      __VITE_ENV__: env
-    },
-    server: {
-      port: 3000,
-      host: true
-    }
-  }
+export default defineConfig({
+  plugins: [
+    react(),
+    visualizer({
+      filename: 'stats.html',
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    })
+  ],
+  server: {
+    port: 3000,
+    host: true
+  },
+  envPrefix: [
+    'VITE_',
+    'SB_SQL_DB_'
+  ]
 })
