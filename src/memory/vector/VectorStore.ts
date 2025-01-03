f8bd8fad5db69da8c2cbc776e70b20b7b7337726
GaryOcean428 <braden.lang77@gmail.com>
2025-01-03 20:10:29 +0800

    Fix ESLint errors

--- src/memory/vector/VectorStore.ts
+++ src/memory/vector/VectorStore.ts
@@ -1,5 +1,5 @@
 import { logger } from '../../lib/utils/logger'
-import { monitoring } from '../../lib/monitoring'
+import { monitoring } from '../../utils/monitoring'
 import {
   VectorMetadata,
   SearchResult,
